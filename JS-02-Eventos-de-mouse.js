75748adf043b059fddc4b5f504f492edb32d9b4e
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-15 19:01:53 -0200

    Eventos de mouse.

---
+++ JS-02-Eventos-de-mouse.js
@@ -0,0 +1,32 @@
+function apertouMouse() {
+    alert("APERTOU O MOUSE");
+}
+
+function soltouMouse() {
+    alert("SOLTOU O MOUSE!");
+}
+
+function emcimaMouse() {
+    alert("VOCÊ PASSOU POR CIMA DO BOTÃO!");
+}
+
+function saiuMouse() {
+    alert("VOCÊ SAIU DE CIMA DO BOTÃO!");
+}
+
+function direitoMouse() {
+    alert("BOTÃO DIREITO DO MOUSE");
+    return false;
+}
+
+function onclickMouse() {
+    alert("VOCÊ DEU UM CLICK COMPLETO");
+}
+
+function mouseMove() {
+    alert("VOCÊ SE MOVEU COM O MOUSE");
+}
+
+function duploCliqueMouse() {
+    alert("VOCÊ DEU UM DUPLO CLICK");
+}
