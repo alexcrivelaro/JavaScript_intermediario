94f5bab691f458893aa14fa7949971572e12b6c2
Alexandre Crivelaro <alexcrivelaro@hotmail.com>
2017-11-16 00:29:31 -0200

    Eventos de formulário.

---
+++ JS-05-Eventos-de-formulario.js
@@ -0,0 +1,12 @@
+function mudouOpcao(objeto) {
+    alert("Selecionou: "+objeto.value);
+
+}
+
+function focou() {
+    console.log("Focou no campo nome");
+}
+
+function desfocou() {
+    console.log("Desfocou do  campo 1");
+}
